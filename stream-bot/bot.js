60ee010da89f3b97a42b8396d6a6582e722b63e2
Jordan Loop and Joshua Fairchild <jordan+joshua@>
2017-06-29 15:34:08 -0700

    add method that alphabetizes a user's tweet and replies to the user with it

--- stream-bot/bot.js
+++ stream-bot/bot.js
@@ -8,14 +8,24 @@ class Bot {
     this.api = new Twit(apiKeys);
   }
 
-  watchUserTweets(userId) {
+  watchUserTweets(userId, replyType) {
     const stream = this.api.stream('statuses/filter', {follow: userId})
       .on('tweet', tweet => {
-        const tweetId = tweet.id_str;
-        const username = tweet.user.screen_name;
-        this.api.post('statuses/update', {in_reply_to_status_id: tweetId, status: `${this.getRandomAnswer(answers)} @${username}`})
-          .then(console.log(`Successful tweet`))
-          .catch(error => console.log(error.stack));
+        if (replyType === 'answer') {
+          const tweetId = tweet.id_str;
+          const username = tweet.user.screen_name;
+          this.api.post('statuses/update', {in_reply_to_status_id: tweetId, status: `${this.getRandomAnswer(answers)} @${username}`})
+            .then(console.log(`Successful tweet`))
+            .catch(error => console.log(error.stack));
+          }
+
+        else if (replyType === 'alphabetize') {
+          const tweetId = tweet.id_str;
+          const username = tweet.user.screen_name;
+          this.api.post('statuses/update', {in_reply_to_status_id: tweetId, status: `${this.alphabetizeTweet(tweet.text)} @${username}`})
+            .then(console.log(`Successful tweet`))
+            .catch(error => console.log(error.stack));
+        }
       })
       .on('error', error => console.log(error.stack));
   }
@@ -24,10 +34,15 @@ class Bot {
     return collection[Math.floor(Math.random()*((collection.length-1)-0+1)+0)];
   }
 
+  alphabetizeTweet(tweet) {
+    return tweet.toLowerCase().split(" ").sort().join(" ");
+  }
+
 }
 
 
 //node execution area
 const bot = new Bot();
 
-bot.watchUserTweets(userIds.content_bot1000);
+// bot.watchUserTweets(userIds.content_bot1000, 'answer');
+bot.watchUserTweets(userIds.EpicodusStudent, 'alphabetize');
