39a44e5e57c261a026d6dd930b0f75435d6c5893
Jordan Loop and Joshua Fairchild <jordan+joshua@>
2017-06-29 15:14:03 -0700

    add generateRandomItem method and begin tweeting proverbs

--- content-bot/bot.js
+++ content-bot/bot.js
@@ -1,6 +1,7 @@
 const apiKeys = require('./.env').apiKeys;
 const Twit = require('twit');
 const questions = require('./questions.json');
+const proverbs = require('./proverbs.json');
 // const tweetRate = 1000*70;
 
 class Bot {
@@ -8,20 +9,42 @@ class Bot {
     this.api = new Twit(apiKeys);
   }
 
-  postTweet() {
-    // const question = this.getRandomQuestion(questions);
-    const question = '12 worms?';
+  postQuestion() {
+    const question = this.generateRandomItem(questions);
+    // const question = '14 worms?';
     this.api.post('statuses/update', {status: question})
       .then(console.log(`Successful tweet:  ${question}`))
+      .catch(error => console.log(`Error occurred:  ${error.stack}`));
+  }
+
+  postProverb() {
+    const proverb = this.generateRandomItem(proverbs);
+    // const proverb = "14 worms?";
+    this.api.post('statuses/update', {status: proverb})
+      .then(console.log(`Successful tweet:  ${proverb}`))
       .catch(error => console.log(`Error occurred:  ${error.stack}`));
   }
 
   getRandomQuestion(collection) {
     return collection[Math.floor(Math.random()*((collection.length-1)-0+1)+0)];
   }
+
+  generateRandomItem(collection) {
+    const rand1 = collection[Math.floor(Math.random()*((collection.length-1)-0+1)+0)].split(" ");
+    const rand2 = collection[Math.floor(Math.random()*((collection.length-1)-0+1)+0)].split(" ");
+
+    const firstHalf = rand2.splice(0, Math.floor(rand2.length / 2)).join(" ");
+    const secondHalf = rand1.splice(Math.floor(rand1.length / 2)).join(" ");
+
+    return`${firstHalf} ${secondHalf}`;
+  }
 }
 
 //node execution area
 const bot = new Bot();
 
-bot.postTweet();
+bot.postProverb();
+// bot.postQuestion();
+
+// setInterval(() => postProverb, tweetRate);
+// setInterval(() => postQuestion, tweetRate);
