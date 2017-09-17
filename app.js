5a16fea4e9cbecc3d6472accd390c0e9b0e1d786
Joshua Fairchild <Joshua.fairchild429@gmail.com>
2017-09-16 19:52:35 -0700

    remove old comment

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ const questions = require('./assets/questions.json');
 const proverbs = require('./assets/proverbs.json');
 const userIds = require('./.env').userIds;
 const tweetRate = 1000*3;
+
 const contentBot = new ContentBot();
 const streamBot = new StreamBot();
 
 
-// bot.postTweet(proverbs);
-// bot.postTweet(questions);
-
 // setInterval(() => contentBot.postTweet(proverbs), tweetRate);
 setInterval(() => contentBot.postTweet(questions), tweetRate);
-
 
 // streamBot.watchUserTweets([userIds.content_bot1000], 'answer');
 streamBot.watchUserTweets([userIds.content_bot1000], 'alphabetize');
